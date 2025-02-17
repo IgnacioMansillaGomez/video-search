4547d3ada3c6dba3d7e10b7cac6ff45ab626e488
IgnacioMansillaGomez <ignaciomansilladeveloper@gmail.com>
2025-02-16 23:05:50 -0300

    ownership improvement

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use yew::{callback, function_component, html, use_state, Callback, Properties, UseStateHandle};
+use std::string;
+
+use wasm_bindgen::{JsCast, UnwrapThrowExt};
+use web_sys::HtmlInputElement;
+use yew::{
+    callback, function_component, html, use_state, Callback, Event, InputEvent, MouseEvent,
+    Properties, UseStateHandle,
+};
 fn main() {
     yew::start_app::<App>();
 }
@@ -11,8 +18,18 @@ struct Video {
 
 #[function_component(App)]
 fn app() -> Html {
-    let on_search = Callback::from(|_| {});
     let video: UseStateHandle<Option<Video>> = use_state(|| None);
+
+    let on_search: Callback<String> = {
+        let video: UseStateHandle<Option<Video>> = video.clone();
+        Callback::from(move |text_to_seach: String| {
+            let video_id: String = search_youtube(text_to_seach);
+            video.set(Some(Video {
+                id: video_id,
+                name: "name".to_string(),
+            }))
+        })
+    };
 
     let video_section = match (*video).clone() {
         Some(video) => html! {
@@ -34,9 +51,31 @@ struct VideoControlProps {
     on_search: Callback<String>,
 }
 
+fn search_youtube(text_to_search: String) -> String {
+    !todo!("HTTP REQUEST");
+    web_sys::console::log_1(&text_to_search.into());
+
+    String::from("algo")
+}
+
 #[function_component(VideoControl)]
 fn video_control(props: &VideoControlProps) -> Html {
-    let handle_input = Callback::from(|_| {});
+    let text_to_search: UseStateHandle<String> = use_state(|| String::new());
+
+    let handle_input: Callback<yew::InputEvent> = {
+        let text_to_search = text_to_search.clone();
+
+        Callback::from(move |input_event: InputEvent| {
+            //get text from input event
+            let text: String = get_value_from_input_event(input_event);
+            text_to_search.set(text)
+        })
+    };
+
+    let handle_search: Callback<MouseEvent> = {
+        let on_search = props.on_search.clone();
+        Callback::from(move |_| on_search.emit(text_to_search.to_string()))
+    };
 
     html! {
         <main>
@@ -47,7 +86,7 @@ fn video_control(props: &VideoControlProps) -> Html {
                 <input type="text" oninput={handle_input} />
             </div>
             <div>
-                <button>{"Search"}</button>
+                <button onclick={handle_search}>{"Search"}</button>
             </div>
         </main>
     }
@@ -61,7 +100,7 @@ struct VideoSectionProps {
 
 #[function_component(VideoSection)]
 fn video_sectionl(props: &VideoSectionProps) -> Html {
-    let yt_url = format!("https://www.youtube.com/embed/{}", props.id);
+    let yt_url: String = format!("https://www.youtube.com/embed/{}", props.id);
     html! {
         <main>
         <div>
@@ -69,4 +108,11 @@ fn video_sectionl(props: &VideoSectionProps) -> Html {
         </div>
         </main>
     }
+}
+
+fn get_value_from_input_event(e: InputEvent) -> String {
+    let event: Event = e.dyn_into().unwrap_throw();
+    let event_target = event.target().unwrap_throw();
+    let target: HtmlInputElement = event_target.dyn_into().unwrap_throw();
+    target.value()
 }
